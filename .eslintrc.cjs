8f8345df306782abfa7a8621a70637747767ed9a
Molly <molly.lin@senao.com.tw>
2023-08-22 15:58:31 +0800

    Build: install @vue/eslint-config-airbnb-with-typescript and fix eslint error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,21 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
   root: true,
   env: {
     node: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'eslint:recommended',
     '@vue/typescript/recommended',
-    'plugin:vue/vue3-essential'
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-airbnb-with-typescript',
   ],
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   rules: {
-  }
-}
+  },
+};
