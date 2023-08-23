ac832b1dffad6f054507c1a145d9328b38dc9e9b
Molly <molly.lin@senao.com.tw>
2023-08-23 15:42:24 +0800

    Refactor: MainContent Component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <script setup lang="ts">
 import Navbar from './components/NavBar.vue';
+import MainContent from './components/MainContent.vue';
 </script>
 
 <template>
   <div class="flex flex-col h-screen">
-    <navbar />
-    <main class="container mx-auto mt-12 prose prose-sm grow">
-      <h1 class="text-center tracking-wide">GitHub Importer Maintenance</h1>
-      <h3 class="text-center tracking-wider mt-6 font-normal">
-        Scheduled Maintenance Report for GitHub
-      </h3>
-      <article class="flex mx-auto mt-12 md:max-w-lg">
-        <section class="flex-auto w-1/3 text-center font-bold">
-          <span>Completed</span>
-        </section>
-        <section class="flex-auto w-2/3">
-          <span>The scheduled maintenance has been completed.</span>
-          <div class="text-xs text-base-content">
-            Posted<span> 6</span> months ago.
-            <time class="text-primary">Feb 09, 2023 - 09:09 UTC</time>
-          </div>
-        </section>
-      </article>
-    </main>
+    <Navbar />
+    <MainContent />
     <footer
       class="footer footer-center p-4 bg-base-300 text-base-content mt-24"
     >
---
+++ src/components/MainContent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="container mx-auto mt-12 prose prose-sm grow">
+    <h1 class="text-center tracking-wide">GitHub Importer Maintenance</h1>
+    <h3 class="text-center tracking-wider mt-6 font-normal">
+      Scheduled Maintenance Report for GitHub
+    </h3>
+    <article class="flex mx-auto mt-12 md:max-w-lg">
+      <section class="flex-auto w-1/3 text-center font-bold">
+        <span>Completed</span>
+      </section>
+      <section class="flex-auto w-2/3">
+        <span>The scheduled maintenance has been completed.</span>
+        <div class="text-xs text-base-content">
+          Posted<span> 6</span> months ago.
+          <time class="text-primary">Feb 09, 2023 - 09:09 UTC</time>
+        </div>
+      </section>
+    </article>
+  </main>
+</template>
