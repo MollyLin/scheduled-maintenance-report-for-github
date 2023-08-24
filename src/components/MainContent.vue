00e103ceef4e5ea2193862f8e5adfa54f147c0e3
Molly <molly.lin@senao.com.tw>
2023-08-24 15:39:15 +0800

    Feat: Draft Fetch github's API wtih TS

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,4 +1,62 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+function http<T>(request: RequestInfo): Promise<T> {
+  return fetch(request)
+    .then((response: Response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      if (error.response && error.response.status && error.response.status === 403) {
+        console.log('無權限');
+      }
+      console.log(error);
+    });
+}
+
+function get<T>(path: string, args: RequestInit = {}): Promise<T> {
+  args.method = 'get';
+  args.headers = {
+    'Content-Type': 'application/json',
+  };
+  return http<T>(new Request(`${path}`, args));
+}
+
+interface CodeStatus {
+  code: number;
+}
+
+interface MaintenRes extends CodeStatus {
+  scheduled_maintenances: MaintenData[];
+}
+
+interface StatusData {
+  status: string;
+  body: string;
+  display_at: string;
+}
+
+interface MaintenData {
+  name: string; // Title
+  updated_at: string; // 最後更新日期
+  shortlink: string; // source link
+  incident_updates: StatusData[];
+}
+
+const fetchScheduledMaintenances = (): void => {
+  const maintenUrl = 'https://www.githubstatus.com/api/v2/scheduled-maintenances.json';
+  get<MaintenRes>(maintenUrl).then((response) => {
+    console.log(response.scheduled_maintenances);
+  });
+};
+
+onMounted(() => {
+  fetchScheduledMaintenances();
+});
+</script>
 
 <template>
   <main class="container mx-auto mt-12 prose prose-sm grow">
