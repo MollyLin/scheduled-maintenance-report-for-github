be095c4193c1bf4f9fcd3071bb02ff6583ec349e
Molly <molly.lin@senao.com.tw>
2023-08-24 15:37:08 +0800

    Build: 調整 lint 與 format 規則

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,13 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-essential',
-    '@vue/eslint-config-airbnb-with-typescript',
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', '@vue/eslint-config-airbnb-with-typescript'],
   parserOptions: {
     parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
   },
   rules: {
+    'no-console': 'off',
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['args'] }],
   },
 };
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   trailingComma: 'es5',
   singleQuote: true,
-  printWidth: 80,
+  printWidth: 100,
   semi: true,
 };
