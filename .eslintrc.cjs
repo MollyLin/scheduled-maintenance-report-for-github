71bcc9d58a468e3c5e4c74176d67a0d5f18613a6
Molly <molly.lin@senao.com.tw>
2023-08-25 15:59:55 +0800

    Build: Ignore destructured names for ts-lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,11 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', '@vue/eslint-config-airbnb-with-typescript'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-airbnb-with-typescript',
+  ],
   parserOptions: {
     parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
@@ -14,5 +18,13 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['args'] }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        modifiers: ['destructured'],
+        format: null,
+      },
+    ],
   },
 };
