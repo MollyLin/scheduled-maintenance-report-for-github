2407ea18b5846cbb89979ed8ac2d499efd89eff0
Molly <molly.lin@senao.com.tw>
2023-08-30 10:48:11 +0800

    refactor: set path resolve alias from vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
-  base: 'scheduled-maintenance-report-for-github',
+  plugins: [vue(), eslintPlugin(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src'),
+      },
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, './src/assets'),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, './src/components'),
+      },
+      {
+        find: '@helpers',
+        replacement: path.resolve(__dirname, './src/helpers'),
+      },
+    ],
+  },
 });
