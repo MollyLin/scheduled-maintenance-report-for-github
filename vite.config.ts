ed6c9d51a93f6330c11fe1f4fab29755de43be71
Molly <mollylin2014@gmail.com>
2023-09-18 12:20:19 +0800

    Fix: fix for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'scheduled-maintenance-report-for-github',
   plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
