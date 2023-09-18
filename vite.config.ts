b9c1fd6b3938b78a2313e4654cd6f2957bba786e
Molly <mollylin2014@gmail.com>
2023-09-18 17:04:10 +0800

    Feature: add Router and set base_url by mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: 'scheduled-maintenance-report-for-github',
-  plugins: [vue(), eslintPlugin()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@helpers': path.resolve(__dirname, 'src/helpers'),
-      '@types': path.resolve(__dirname, 'src/types'),
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+  return defineConfig({
+    base: process.env.BASE_URL,
+    plugins: [vue(), eslintPlugin()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@helpers': path.resolve(__dirname, 'src/helpers'),
+        '@types': path.resolve(__dirname, 'src/types'),
+      },
     },
-  },
-});
+  });
+};
