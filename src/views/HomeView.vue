4927379f97c54c411848ec4d7603be8efa71b24e
Molly <mollylin2014@gmail.com>
2023-09-21 14:40:05 +0800

    set temp page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <pre>{{ result }}</pre>
+  </div>
+</template>
+
+<script async setup lang="ts">
+import { ref } from 'vue';
+
+const result = ref();
+
+const response = await fetch('https://api.publicapis.org/entries');
+result.value = await response.json();
+</script>
